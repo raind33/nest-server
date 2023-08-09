6924647262fa69405a61239287a6765356a00b4a
wuyuliang <2331336537@qq.com>
2023-08-09 17:20:32 +0800

    feat: pure login and register jwt

---
+++ src/user/user.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+  Res,
+  ValidationPipe,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+
+@Controller('user')
+export class UserController {
+  @Inject(JwtService)
+  private readonly jwtService: JwtService;
+  constructor(private readonly userService: UserService) {}
+
+  @Post('login')
+  async login(
+    @Body(ValidationPipe) user: LoginDto,
+    @Res({ passthrough: true }) res: any,
+  ) {
+    const foundUser = await this.userService.login(user);
+
+    if (foundUser) {
+      const token = await this.jwtService.signAsync({
+        user: {
+          id: foundUser.id,
+          username: foundUser.username,
+        },
+      });
+      res.setHeader('token', token);
+      return 'login success';
+    } else {
+      return 'login fail';
+    }
+  }
+
+  @Post('register')
+  register(@Body(ValidationPipe) user: RegisterDto) {
+    return this.userService.register(user);
+  }
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, HttpException, Logger } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import * as crypto from 'crypto';
+
+function md5(str) {
+  const hash = crypto.createHash('md5');
+  hash.update(str);
+  return hash.digest('hex');
+}
+@Injectable()
+export class UserService {
+  @InjectRepository(User)
+  private readonly userRepository: Repository<User>;
+  private logger = new Logger();
+
+  async register(user: RegisterDto) {
+    const foundUser = await this.userRepository.findOneBy({
+      username: user.username,
+    });
+
+    if (foundUser) {
+      throw new HttpException('用户已存在', 200);
+    }
+
+    const newUser = new User();
+    newUser.username = user.username;
+    newUser.password = md5(user.password);
+
+    try {
+      await this.userRepository.save(newUser);
+      return '注册成功';
+    } catch (e) {
+      this.logger.error(e, UserService);
+      return '注册失败';
+    }
+  }
+  async login(user: LoginDto) {
+    const foundUser = await this.userRepository.findOneBy({
+      username: user.username,
+    });
+
+    if (!foundUser) {
+      throw new HttpException('用户名不存在', 200);
+    }
+    if (foundUser.password !== md5(user.password)) {
+      throw new HttpException('密码错误', 200);
+    }
+    return foundUser;
+  }
+}
